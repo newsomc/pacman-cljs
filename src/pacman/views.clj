90f0fd01a2d2a2dbb316c4595c1f0b560e3d2a0f
Clint Newsom <hcnewsom@gmail.com>
2013-03-11 15:16:52 -0400

    title update

--- src/pacman/views.clj
+++ src/pacman/views.clj
@@ -7,7 +7,7 @@
      [:title "Pacman!"]
      (include-css "/css/style.css")]
     [:body
-     [:h1 "Pacman!"]
+     [:h1 "Cljs Pacman!"]
      [:div {:id "pacman"}]
      ]
     (include-js "/js/jquery-1.9.1.min.js" "/js/modernizr.custom.95208.js"  "/js/cljs.js")))
