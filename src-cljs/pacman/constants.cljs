8517179d8d56ded82a7e7e05f0dcd54594bcbb00
Clint Newsom <hcnewsom@gmail.com>
2013-03-20 11:02:40 -0400

    formatting

--- src-cljs/pacman/constants.cljs
+++ src-cljs/pacman/constants.cljs
@@ -3,19 +3,19 @@
 (defn console-log [var]
   (.log js/console (pr-str var)))
 
-(def game-const {:none 4
-                 :up 3
-                 :left 2
-                 :down 1
-                 :right 11
-                 :waiting 5
-                 :pause 6
-                 :playing 7
-                 :countdown 8
-                 :eaten-pause 9
-                 :dying 10
-                 :pacman {}
-                 })
+(def game-const 
+  {:none 4
+   :up 3
+   :left 2
+   :down 1
+   :right 11
+   :waiting 5
+   :pause 6
+   :playing 7
+   :countdown 8
+   :eaten-pause 9
+   :dying 10 })
+
 (def FPS 30)
 (def WAITING true)
 (def WALL 0)
@@ -23,7 +23,6 @@
 (def EMPTY 2)
 (def BLOCK 3)
 (def PILL 4)
-
 (def KEY (atom {:keys {:BACKSPACE 8, :TAB 9, :NUM_PAD_CLEAR 12, :ENTER 13, :SHIFT 16, :CTRL 17, :ALT 18, :PAUSE 19, :CAPS_LOCK 20, :ESCAPE 27, :SPACEBAR 32, :PAGE_UP 33, :PAGE_DOWN 34, :END 35, :HOME 36, :ARROW_LEFT 37, :ARROW_UP 38, :ARROW_RIGHT 39, :ARROW_DOWN 40, :PRINT_SCREEN 44, :INSERT 45, :DELETE 46, :SEMICOLON 59, :WINDOWS_LEFT 91, :WINDOWS_RIGHT 92, :SELECT 93, :NUM_PAD_ASTERISK 106, :NUM_PAD_PLUS_SIGN 107, :NUM_PAD_HYPHEN-MINUS 109, :NUM_PAD_FULL_STOP 110, :NUM_PAD_SOLIDUS 111, :NUM_LOCK 144, :SCROLL_LOCK 145, :EQUALS_SIGN 187, :COMMA 188, :HYPHEN-MINUS 189, :FULL_STOP 190, :SOLIDUS 191, :GRAVE_ACCENT 192, :LEFT_SQUARE_BRACKET 219, :REVERSE_SOLIDUS 220, :RIGHT_SQUARE_BRACKET 221, :APOSTROPHE 222}}))
 
 ((fn []
