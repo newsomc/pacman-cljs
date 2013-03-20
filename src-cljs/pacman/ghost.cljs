e84b442c8f65bd068793a0cf43f64f212f65a431
Clint Newsom <hcnewsom@gmail.com>
2013-03-20 11:15:37 -0400

    mostly formatting related changes

--- src-cljs/pacman/ghost.cljs
+++ src-cljs/pacman/ghost.cljs
@@ -20,20 +20,17 @@
   (and (= (:eatable ghost) nil)
        (not= (:eaten ghost) nil)))
 
-(defn current-speed-logic 
-  "Dir is the current direction, x and y are constant units, speed is variable."
-  [dir x y speed]
-  (if (= x :left) (or (and (== dir x) (cljs.core/unchecked-negate speed))
-                      (and (or (== dir y) speed) 0))
-      (or (and (== dir x) speed)
-          (and (or (== dir y)(cljs.core/unchecked-negate speed)) 0))))
-
 (defn get-new-coord 
-  ""
   [ghost dir current]
   (let [speed (if (= (is-vulnerable? ghost) true) (if (is-hidden? ghost) 4 2))
-        x-speed (current-speed-logic dir :left speed :right)
-        y-speed (current-speed-logic dir :down speed :up)]
+        x-speed (or 
+                  (and (= dir (:left const/game-const)) (- speed))
+                  (and (= dir (:right const/game-const)) speed) 
+                  0)
+        y-speed (or 
+                  (and (= dir (:down const/game-const)) speed)
+                  (and (= dir (:up const/game-const)) (- speed)) 
+                  0)]
 
     {:x (add-bounded (:x current) x-speed) 
      :y (add-bounded (:y current) y-speed)}))
@@ -49,22 +46,20 @@
                 [(:left const/game-const) (:right const/game-const)])]))
 
 ;; =====================================================
-;; Ghost state
+;; Reset ghost state
+;; =====================================================
+(defn ghost-init-state [ghost] 
+  {:eaten nil, 
+   :eatable nil, 
+   :position {:x 90, :y 80}
+   :direction (get-random-direction ghost)
+   :due (get-random-direction ghost)})
 
-(defn ghost-init-state [ghost] {:eaten true, 
-                                :eatable true, 
-                                :position {:x 90, :y 80}
-                                :direction (get-random-direction ghost)
-                                :due (get-random-direction ghost)})
-
-(defn reset-ghost
-  [ghost]
+(defn reset-ghost [ghost]
   (merge-with merge ghost (ghost-init-state ghost)))
 
-(defn reset-ghost-state! [ghosts]
-  (helper/console-log "hi")
-  (helper/console-log ghosts)
-  #_(swap! @state/game-state (fn [state] (update-in ghosts [:ghosts] #(map reset-ghost ghosts)))))
+(defn reset-state! []
+  (swap! state/game-state update-in [:ghosts] #(map reset-ghost %)))
 
 ;; =====================================================
 
@@ -78,13 +73,29 @@
       (== dir [(:right const/game-const) (:left const/game-const)])
       (or (== dir [(:up const/game-const) (:down const/game-const)]) (:up const/game-const))))
 
-(defn make-eatable [ghost]
-    (swap! ghost assoc-in [:direction] (opposite-direction (:direction ghost)))
-    (swap! ghost assoc-in [:eatable] helper/get-tick))
+;; =====================================================
+;; Make ghost eatable
 
-(defn eat [ghost]
-  (swap! ghost assoc-in [:eatble] nil)
-  (swap! ghost assoc-in [:eaten] helper/get-tick))
+(defn set-eatable-data [ghost]
+  {:direction (opposite-direction (:direction ghost))
+   :eatable (helper/get-tick)})
+
+(defn merge-ghost-eatable [ghost]
+  (merge-with merge ghost (ghost-init-state ghost)))
+
+(defn make-eatable! []
+  (swap! state/game-state update-in [:ghosts] #(map merge-ghost-eatable %)))
+
+;; =====================================================
+
+#_(defn make-eatable []
+    (swap! state/game-state assoc-in [:direction] (opposite-direction (:direction ghost)))
+    (swap! state/game-state assoc-in [:eatable] helper/get-tick))
+
+;; This doesn't work.
+(defn eat []
+  (swap! state/game-state assoc-in [:eatble] nil)
+  (swap! state/game-state assoc-in [:eaten] helper/get-tick))
 
 (defn point-to-cord [x]
   (.round js/Math (/ x 10)))
@@ -97,15 +108,10 @@
      (or (== dir (:right const/game-const))
          (== dir (:down const/game-const)))(+ x (- 10 rem)))
     :else (- x rem)))
-
-(defn on-grid-square 
-  ""
-  [pos]
+(defn on-grid-square [pos]
   (and (on-whole-square (:y pos)) (on-whole-square (:x pos))))
 
-(defn seconds-ago 
-  ""
-  [tick]
+(defn seconds-ago [tick]
   (/ (- helper/get-tick tick) const/FPS))
 
 ;; What is color? Need to define it!
@@ -117,9 +123,10 @@
    (if (:eaten ghost) "#222")
    :else (:color ghost)))
 
-(defn draw [ghost ctx]
-  (let [position (:position ghost)
-        s (:block-size (:map ghost))
+(defn draw [ghost]
+  (let [ctx (:ctx @state/game-state)
+        position (:position ghost)
+        s (:block-size @gamemap/map-state)
         eatable (:eatable ghost)
         top (* (/ (:y position) 10) 2)
         left (* (/ (:x position) 10) 2)
@@ -131,21 +138,24 @@
         direction (:direction ghost)
         f (/ s 12)
         off {}]
+
     (cond 
      (and eatable (> (seconds-ago eatable) 8)) (swap! ghost assoc-in [:eatable] nil)
      (and eatable (> (seconds-ago eatable) 3)) (swap! ghost assoc-in [:eaten] nil))
-   
-    (set! (. ctx -fillStyle) (get-color ghost))
+    
+    (set! (. ctx -fillStyle) (:specs ghost))
+    ;(set! (. ctx -fillStyle) (get-color ghost))
+
     (.beginPath ctx)
     (.moveTo ctx left base)
+
     (.quadraticCurveTo ctx left top (+ left (/ s 2)) top)
     (.quadraticCurveTo ctx (+ left s) top (+ left s) base)
-    
-    (.quadraticCurveTo (- tl (* inc 1)) (+ base high) (- (* inc 2) tl) base)
-    (.quadraticCurveTo (- tl (* inc 3)) (+ base low)  (- (* inc 4) tl) base)
-    (.quadraticCurveTo (- tl (* inc 5)) (+ base high) (- (* inc 6) tl) base)
-    (.quadraticCurveTo (- tl (* inc 7)) (+ base low)  (- (* inc 8) tl) base)
-    (.quadraticCurveTo (- tl (* inc 9)) (+ base low)  (- (* inc 8) tl) base)
+    (.quadraticCurveTo ctx (- tl (* inc 1)) (+ base high) (- (* inc 2) tl) base)
+    (.quadraticCurveTo ctx (- tl (* inc 3)) (+ base low)  (- (* inc 4) tl) base)
+    (.quadraticCurveTo ctx (- tl (* inc 5)) (+ base high) (- (* inc 6) tl) base)
+    (.quadraticCurveTo ctx (- tl (* inc 7)) (+ base low)  (- (* inc 8) tl) base)
+    (.quadraticCurveTo ctx (- tl (* inc 9)) (+ base low)  (- (* inc 8) tl) base)
     
     (.closePath ctx)
     (.fill ctx)
@@ -156,39 +166,50 @@
     (.closePath ctx)
     (.fill ctx)
     
-    (conj off {:right [f 0] :left [(cljs.core/unchecked-negate f)  0] :up [0 (cljs.core/unchecked-negate f)] :down [0 f]})
+    (conj off {:right [f 0] :left [(- f)  0] :up [0 (- f)] :down [0 f]})
 
     (.beginPath ctx)
     (set! (. ctx -fillStyle) "#000")
-    (.arc ctx (+ left 6 (nth (direction off) 0)) (+ left 6 (nth (direction off) 1)) (/ s 15) 0 300 false)
-    (.arc ctx (+ (- (+ left s) 6) (nth (direction off) 0)) (+ left 6 (nth (direction off) 1)) (/ s 15)  300 false)
+    (.arc ctx (+ left 6 (nth ((keyword direction) off) 0)) (+ left 6 (nth ((keyword direction) off) 1)) (/ s 15) 0 300 false)
+    (.arc ctx (+ (- (+ left s) 6) (nth ((keyword direction) off) 0)) (+ top 6 (nth ((keyword direction) off) 1)) (/ s 15) 0 300 false)
     (.closePath ctx)
     (.fill ctx)))
 
-(defn pane [ghost pos]
+(defn pane [pos dir]
   (cond 
-   (and (= (:y pos) 100) ( >= (:x pos) 190) (= (:direction ghost) (:right const/game-const)) {:y 100 :x -10}) 
-   (and (= (:y pos) 100) (<= (:x pos) -10) (= (:direction ghost) (:left const/game-const)) {:y 100 :x 190})))
+   (and (= (:y pos) 100) (>= (:x pos) 190) (= dir (:right const/game-const))) {:y 100 :x -10}
+   (and (= (:y pos) 100) (<= (:x pos) -10) (= dir (:left const/game-const))) {:y 100 :x 190}))
 
-(defn move-ghost [ghost due on-grid]
-  (swap! ghost assoc-in [:npos] (get-new-coord ghost due (:position ghost)))
-  (if (and (on-grid) (map/is-floor-space {:y (helper/point-to-coord (next-square (:y (:npos ghost)) due)) 
-                                          :x (helper/point-to-coord (next-square (:x (:npos ghost)) due))})) 
-    (swap! ghost assoc-in [:direction] due)))
+;; ====================================================
+;; Move ghost - experimental
+;; ====================================================
 
-(defn move [ghost ctx]
-  (let [direction (:direction ghost)
-        old-pos (:position ghost)
-        on-grid (on-grid-square (:position ghost))
-        due (:due ghost)
-        tmp (pane ghost (:position ghost))]
-    (if (not= due direction) 
-      (move-ghost ghost due on-grid)
-      (swap! ghost assoc-in [:due] nil))
-    (if (nil? (:npos ghost)) (swap! ghost assoc-in [:npos] (get-new-coord ghost (:direction ghost) (:position ghost))))
-    (if (and (on-grid) (map/is-wall-space {:y (helper/point-to-coord (next-square (:y (:npos ghost)) direction) ) 
-                                           :x (helper/point-to-coord (next-square (:x (:npos ghost)) direction))}))
-      (swap! ghost assoc-in [:due] (get-random-direction ghost)))
-    (swap! ghost assoc-in [:position] (:nps ghost))
-    (if-not (nil? tmp) (swap! ghost assoc-in [:position] tmp))
-    (swap! ghost assoc-in [:due] (get-random-direction ghost))))
+(defn get-npos [ghost]
+  (cond (nil? (:npos ghost)) (get-new-coord ghost (:direction ghost) (:position ghost))
+        (not= (:due ghost) (:direction ghost)) (get-new-coord ghost (:due ghost) (:position ghost))))
+
+(defn tmp-pos [ghost]
+  (if-let [tmp (pane (:position ghost) (:direction ghost))]
+           tmp))
+
+(defn change-direction [ghost]
+  (let [due (:due ghost)
+        npos (get-new-coord ghost due (:position ghost))]
+    (if (and (on-grid-square (:position ghost)) (gamemap/is-floor-space? {:y (helper/point-to-coord (next-square (:y (:npos ghost)) due)) 
+                                                                          :x (helper/point-to-coord (next-square (:x (:npos ghost)) due))}))
+      (:due ghost)
+      nil)))
+
+(defn reset-ghost-direction! [ghost] 
+  {:old-pos (:position ghost)
+   :npos (get-npos ghost)
+   :position (if (nil? (tmp-pos ghost)) (:due ghost) (tmp-pos ghost)) 
+   :direction (change-direction ghost)
+   :due (change-direction ghost)})
+
+(defn ghost-moves [ghost]
+  (merge-with merge ghost (reset-ghost-direction! ghost)))
+
+(defn move! []
+  (swap! state/game-state update-in [:ghosts] #(map ghost-moves %)))
+
