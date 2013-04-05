7cbfb34ac28b42e22b8b80a3a4419b433857cff0
Clint Newsom <hcnewsom@gmail.com>
2013-04-05 14:07:29 -0400

    Clearing up bug with macros

--- src/pacman/views.clj
+++ src/pacman/views.clj
@@ -8,8 +8,8 @@
      (include-css "/css/style.css")]
     [:body
      [:h1 "Cljs Pacman!"]
-     [:div {:id "pacman"}
-       [:canvas {:id "canvas"}]]]
+      [:div {:id "pacman"}
+        [:canvas {:id "canvas"}]]]
     (include-js "/js/jquery-1.9.1.min.js" "/js/modernizr.custom.95208.js"  "/js/cljs.js")))
 
 
