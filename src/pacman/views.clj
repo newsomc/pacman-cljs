61040b9048f4356ade2d884d97d4dba1ff5d0a4d
Clint Newsom <hcnewsom@gmail.com>
2013-04-30 14:40:12 -0400

    Rename compiled clojurescript file from cljs.js to pacman.js; remove pacman.js from repo.

--- src/pacman/views.clj
+++ src/pacman/views.clj
@@ -10,7 +10,7 @@
      [:h1 "Cljs Pacman!"]
       [:div {:id "pacman"}
         [:canvas {:id "canvas"}]]]
-    (include-js "/js/jquery-1.9.1.min.js" "/js/modernizr.custom.95208.js"  "/js/cljs.js")))
+    (include-js "/js/jquery-1.9.1.min.js" "/js/pacman.js")))
 
 
 
