1046bf882dd7a22e735b461ce7cc4425c50122bb
Clint Newsom <hcnewsom@gmail.com>
2013-04-30 14:34:41 -0400

    Removing unneeded deps.

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
 (defproject pacman "0.1.0-SNAPSHOT"
-  :description "Pacman for cljs!"
-  :url "http://example.com/FIXME"
+  :description "An almost functional purely functional Pac-Man!"
+  :url "http://pacmancljs.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                   [compojure "1.1.5"]
-                  [hiccup "1.0.2"]
-                  [jayq "2.1.0"]]
+                  [hiccup "1.0.2"]]
   :plugins [[lein-ring "0.8.2"]
-             [lein-beanstalk "0.2.7"]
-             [lein-cljsbuild "0.3.0"]]
-  ;:source-paths ["src-cljs"]
+            [lein-cljsbuild "0.3.0"]]
+  ;; enable if you want macros!
+  ;; :source-paths ["src-cljs"]
   :ring {:handler pacman.routes/app}
   :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
   :cljsbuild {:builds
@@ -17,6 +16,6 @@
                   :compiler
                   {:pretty-print true,
                     :output-to "resources/public/js/pacman.js",
-                    :optimizations :simple}}],
+                    :optimizations :advanced}}],
                :repl-listen-port 9000}
   :main pacman.routes)
